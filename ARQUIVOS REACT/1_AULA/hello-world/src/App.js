26f823847e0512c460e1e532e22711ceb0221b71
Nauana da Silva Oliveira <nauana.oliveira12@gmail.com>
2026-08-27 16:26:42 -0500

    Primeiras aulas do react

---
+++ ARQUIVOS REACT/1_AULA/hello-world/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Olá !</h1>
+      <h2>Emmet funcionou!</h2>
+      <p> Tarefa n°1</p>
+     
+    </div>
+  );
+}
+
+export default App;
